c5248c7461e7a69bee0b6f3ec120ef80df6535c5
Tayo <l.lee@tayo.ws>
2017-11-18 00:51:24 +0100

    Add simple logging system

---
+++ TAGGER/Utils/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TAGGER.Utils
+{
+    class Logger
+    {
+        public static async Task Exception(string message)
+        {
+            StreamWriter log;
+
+            if (!Directory.Exists("Logs"))
+                Directory.CreateDirectory("Logs");
+
+            log = new StreamWriter("Logs/Exceptions.txt");
+            await log.WriteLineAsync(DateTime.Now.ToString() + message);
+        }
+
+        public static async Task Warning(string message)
+        {
+            StreamWriter log;
+
+            if (!Directory.Exists("Logs"))
+                Directory.CreateDirectory("Logs");
+
+            log = new StreamWriter("Logs/Warnings.txt");
+            await log.WriteLineAsync(DateTime.Now.ToString() + message);
+        }
+
+        public static async Task Verbose(string message)
+        {
+            StreamWriter log;
+
+            if (!Directory.Exists("Logs"))
+                Directory.CreateDirectory("Logs");
+
+            log = new StreamWriter("Logs/Verbose.txt");
+            await log.WriteLineAsync(DateTime.Now.ToString() + message);
+        }
+    }
+}
